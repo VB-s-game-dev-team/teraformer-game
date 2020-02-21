78e3cd5b4ed91bb4df8f949072f4224cca2f7fb1
Vojtěch Březina <26902688+VojtechBrezina@users.noreply.github.com>
2020-02-21 21:01:19 +0100

    plaz button switches to planet screen

--- TitleScreen/MainMenu.gd
+++ TitleScreen/MainMenu.gd
@@ -3,5 +3,12 @@ extends MarginContainer
 func _on_QuitButton_pressed():
 	call_deferred("quit")
 
+func _on_Button_pressed():
+	switch_to_planet()
+
 func quit():
 	get_tree().quit()
+	
+func switch_to_planet():
+	get_parent().set_screen("Planet")
+
