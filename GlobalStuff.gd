05582eba80931defaad0c83926beaa6fb2fae889
Vojtěch Březina <26902688+VojtechBrezina@users.noreply.github.com>
2020-02-22 09:42:41 +0100

    passing "" as the screen name closes the game peacefully

--- GlobalStuff.gd
+++ GlobalStuff.gd
@@ -10,6 +10,13 @@ func set_screen(path):
 		remove_child(screen)
 		screen.call_deferred("free")
 	
+	if path == "":
+		call_deferred("quit")
+		return
+	
 	var screen_resource = load("res://" + path + "/" + path + ".tscn")
 	screen = screen_resource.instance()
 	add_child(screen)
+
+func quit():
+	get_tree().quit()
--- TitleScreen/MainMenu.gd
+++ TitleScreen/MainMenu.gd
@@ -1,13 +1,10 @@
 extends MarginContainer
 
 func _on_QuitButton_pressed():
-	call_deferred("quit")
+	get_parent().set_screen("")
 
 func _on_PlayButton_pressed():
 	switch_to_planet()
-
-func quit():
-	get_tree().quit()
 	
 func switch_to_planet():
 	get_parent().set_screen("Planet")
