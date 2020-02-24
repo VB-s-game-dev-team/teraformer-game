b7edfd31a51ef0a79769b4ff561a9e9680ae7fd1
Vojtěch Březina <26902688+VojtechBrezina@users.noreply.github.com>
2020-02-24 16:51:03 +0100

    load/save of global variables works (when you quit the desired way)

--- GlobalStuff.gd
+++ GlobalStuff.gd
@@ -19,6 +19,7 @@ signal level_changed
 var _screen: Node = null
 
 func _ready() -> void:
+	load_state()
 	set_screen("TitleScreen")
 
 # Switch to a new screen
@@ -38,7 +39,42 @@ func set_screen(path: String) -> void:
 	add_child(_screen)
 
 func _quit() -> void:
+	save_state()
 	get_tree().quit()
+
+#save/load
+func save_state() -> void:
+	var data := {
+		"cosmic_credits_count": cosmic_credits_count,
+		"star_dust_count": star_dust_count,
+		"experience": experience,
+		"level": level
+	}
+	var save_file := File.new()
+	save_file.open("user://save.dat", File.WRITE)
+	save_file.store_line(to_json(data))
+	save_file.close()
+
+func load_state() -> void:
+	var save_file := File.new()
+	var data: Dictionary = {}
+	
+	if not save_file.file_exists("user://save.dat"):
+		data = {
+			"cosmic_credits_count": 0,
+			"star_dust_count": 0,
+			"experience": 0,
+			"level": 0
+		}
+	else:
+		save_file.open("user://save.dat", File.READ)
+		data = parse_json(save_file.get_line())
+		save_file.close()
+	
+	cosmic_credits_count = data["cosmic_credits_count"] as int
+	star_dust_count = data["star_dust_count"] as int
+	experience = data["experience"] as int
+	level = data["level"] as int
 
 #setters and getters
 func _set_cosmic_credits_count(v: int) -> void:
