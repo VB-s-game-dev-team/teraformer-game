52530cbb02d455e9982e1e7a07a48d69f785ef22
Vojtěch Březina <26902688+VojtechBrezina@users.noreply.github.com>
2020-02-24 20:07:38 +0100

    added building levels

--- GlobalStuff.gd
+++ GlobalStuff.gd
@@ -8,15 +8,21 @@ var star_dust_count: int setget _set_star_dust_count
 var experience: int setget _set_experience
 var level: int setget _set_level
 
+#global arrays
+var _building_level: Array
+
 
 #signals for global variables
 signal cosmic_credits_count_changed
 signal star_dust_count_changed
 signal experience_changed
 signal level_changed
+signal building_level_changed
 
 
 var _screen: Node = null
+
+var _SAVE_VERSION := 1
 
 func _ready() -> void:
 	get_tree().set_auto_accept_quit(false)
@@ -53,10 +59,12 @@ func _quit() -> void:
 #save/load
 func _save_state() -> void:
 	var data := {
+		"version": _SAVE_VERSION,
 		"cosmic_credits_count": cosmic_credits_count,
 		"star_dust_count": star_dust_count,
 		"experience": experience,
-		"level": level
+		"level": level,
+		"building_level": _building_level
 	}
 	var save_file := File.new()
 	save_file.open("user://save.dat", File.WRITE)
@@ -65,24 +73,26 @@ func _save_state() -> void:
 
 func _load_state() -> void:
 	var save_file := File.new()
-	var data: Dictionary = {}
 	
 	if not save_file.file_exists("user://save.dat"):
-		data = {
-			"cosmic_credits_count": 0,
-			"star_dust_count": 0,
-			"experience": 0,
-			"level": 0
-		}
+		cosmic_credits_count = 0
+		star_dust_count = 0
+		experience = 0
+		level = 0
 	else:
 		save_file.open("user://save.dat", File.READ)
-		data = parse_json(save_file.get_line())
+		var data := parse_json(save_file.get_line()) as Dictionary
 		save_file.close()
-	
-	cosmic_credits_count = data["cosmic_credits_count"] as int
-	star_dust_count = data["star_dust_count"] as int
-	experience = data["experience"] as int
-	level = data["level"] as int
+		var version := data["version"] as int
+		
+		if(version >= 0):
+			cosmic_credits_count = data["cosmic_credits_count"] as int
+			star_dust_count = data["star_dust_count"] as int
+			experience = data["experience"] as int
+			level = data["level"] as int
+		
+		if(version >= 1):
+			_building_level = data["building_level"]
 
 #setters and getters
 func _set_cosmic_credits_count(v: int) -> void:
@@ -100,3 +110,11 @@ func _set_experience(v: int) -> void:
 func _set_level(v: int) -> void:
 	level = v
 	emit_signal("level_changed")
+
+#the array ones
+func set_building_level(index: int, value: int) -> void:
+	_building_level[index] = value
+	emit_signal("building_level_changed")
+
+func get_building_level(index: int) -> int:
+	return _building_level[index]
