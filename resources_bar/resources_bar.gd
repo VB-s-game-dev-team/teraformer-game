e17fb863cc3296f2d9ad228bd1be9c4a0f967961
Filipino9507 <fildil111@gmail.com>
2020-02-24 17:34:07 +0100

    Resource variables updating on r. bar and main menu button

--- resources_bar/resources_bar.gd
+++ resources_bar/resources_bar.gd
@@ -1,12 +1,19 @@
 extends Control
 
+var _global_stuff: GlobalStuff
+
 onready var CosmicCreditsValue = $CosmicCreditsValue
 onready var StarDustValue = $StarDustValue
 onready var LevelValue = $LevelValue
 onready var XPValue = $XPValue
 
 func _ready() -> void:
-	pass
+	_global_stuff = get_parent() as GlobalStuff
+	CosmicCreditsValue.text = _global_stuff.cosmic_credits_count
+	StarDustValue.text = _global_stuff.star_dust_count
+	LevelValue.text = _global_stuff.level
+	XPValue.text = _global_stuff.experience
+	
 # Sets the values according to the global variables
 # Names like cosmic credits and star dust are temporary, but in any case:
 # star dust ... more rare / premium currency
