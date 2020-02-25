f4995feb363646358028bf7348dc5995643af088
Vojtěch Březina <26902688+VojtechBrezina@users.noreply.github.com>
2020-02-25 08:43:41 +0100

    mining screen

--- MiningScreen/MiningScreen.gd
+++ MiningScreen/MiningScreen.gd
@@ -4,6 +4,7 @@ var _global_stuff: GlobalStuff
 
 func _ready() -> void:
 	_global_stuff = get_parent() as GlobalStuff
+	$VBoxContainer/ResourcesBar.setup(_global_stuff)
 
 func _on_BackButton_pressed():
 	_global_stuff.set_screen("Planet")
