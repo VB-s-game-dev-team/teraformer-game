e6f2c0385e56fbb502375b8621b9c182d4e5c6f8
Filipino9507 <fildil111@gmail.com>
2020-02-25 14:09:06 +0100

    Tomáš Douda

--- Planet/building_spot/building_spot.gd
+++ Planet/building_spot/building_spot.gd
@@ -3,26 +3,34 @@ extends Control
 var _global_stuff: GlobalStuff
 
 const _upgrade_cost_increase_rate := 1.2
-const building_names := {
-	0: "Building1",
-	1: "Building2",
-	2: "Building3",
-	3: "Building4",
-	4: "Building5",
-	5: "Building6",
-	6: "Suicide Drone Lab",
-	7: "Drill Dome",
-	8: "Big Bomb Factory",
-	9: "Building10",
-	10: "Building11",
-	11: "Building12",
-	12: "Building13",
-	13: "Building14",
-	14: "Building15",
-	15: "Building16",
-	16: "Building17",
-	17: "Building18",
-}
+const building_names := [
+	"Building1",
+	"Building2",
+	"Building3",
+	"Building4",
+	"Building5",
+	"Building6",
+	"Suicide Drone Lab",
+	"Drill Dome",
+	"Big Bomb Factory",
+	"Building10",
+	"Building11",
+	"Building12",
+	"Building13",
+	"Building14",
+	"Building15",
+	"Building16",
+	"Building17",
+	"Building18",
+]
+
+const img_path := "res://Planet/building_spot/building_images/"
+const img_suffix := ".png"
+
+var _building_images := []
+
+signal open_buy_menu()
+
 var _building_prices := [
 	0, 0, 0, 0, 0, 0,
 	1000, 2000, 5000, 0,
@@ -34,6 +42,9 @@ onready var BuyMenu = $BuyMenu
 
 func _ready() -> void:
 	_global_stuff = get_parent() as GlobalStuff
+	_building_images.resize(len(building_names))
+	for i in range(len(building_names)):
+		_building_images[i] = load(img_path + building_names[i] + img_suffix)
 
 func _on_BuyButton_button_up() -> void:
 	get_tree().paused = true
@@ -52,9 +63,7 @@ func _on_AcceptButton_button_up() -> void:
 	var ccc := _global_stuff.cosmic_credits_count
 	if price <= ccc:
 		_global_stuff._set_cosmic_credits_count(ccc - price)
-		$BuildingSpotTexture.texture = \
-		load("res://Planet/building_spot/building_images/" + \
-		building_names[options.selected] + ".png")
+		$BuildingSpotTexture.texture = _building_images[options.selected]
 		_building_prices[options.selected] *= _upgrade_cost_increase_rate
 		
 		# Future change of said variable
