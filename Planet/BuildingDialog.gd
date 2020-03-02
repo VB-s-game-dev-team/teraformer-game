b84926c8eab3816924561e6278064a7466dc3344
Filipino9507 <fildil111@gmail.com>
2020-03-02 19:47:47 +0100

    Better building description and its logic

--- Planet/BuildingDialog.gd
+++ Planet/BuildingDialog.gd
@@ -1,37 +1,63 @@
 extends Control
 
+# Variables pulled from elsewhere and used here
 var selected_idx = -1
 var _selected_price: int
 var _selected_name: String
 var _selected_description: String
+var _selected_required_level: int
+var _selected_my_level: int
 
+# Other nodes
 onready var Planet := get_parent().get_parent().get_parent()
 onready var BuildingSpots := get_parent().get_node("ViewportContainer/Viewport/PlanetRotationNode/BuildingSpots")
 
-onready var BuildingInfo := $BuildingInfo
+onready var BuildingInfo1 := $BuildingInfo1
+onready var BuildingInfo2 := $BuildingInfo2
 
 onready var NotEnoughMoneyMessage := $BuyMenu/NotEnoughMoneyMessage
 onready var NotEnoughLevelMessage := $BuyMenu/NotEnoughLevelMessage
 onready var MaxLevelMessage := $BuyMenu/MaxLevelMessage
 
-var _info_text := "Do you wish to buy this building? \nName: %s \nPrice: %s \nDescription: %s"
+# Info text strings
+var _info_text1 := "Do you wish to buy this building? \nName: %s \nDescription: %s"
+var _info_text2 := "Price: %s \nRequired level: %s \n%s"
 
+# Ready
 func _ready() -> void:
 	for bs in BuildingSpots.get_children():
 		bs.connect("open_buy_menu", self, "_on_BuildingSpot_open_buy_menu")
-		
-func _set_building_variables():
+
+# Setting all the variables from elsewhere		
+func _set_building_variables() -> void:
 	_selected_price = Planet.building_prices[selected_idx]
 	_selected_name = Planet.building_spot_names[selected_idx]
 	_selected_description = Planet.building_descriptions[selected_idx]
-	BuildingInfo.text = _info_text % [_selected_name, _selected_price, _selected_description]
+	_selected_my_level = Planet.global_stuff.get_building_level(selected_idx)
+	
+	if len(Planet.building_level_requirements[selected_idx]) > _selected_my_level + 1:
+		_selected_required_level = Planet.building_level_requirements[selected_idx][_selected_my_level + 1]
+	else:
+		_selected_required_level = -1  # Means max level
+
+# Setting building info text
+func _set_building_info_text() -> void:
+	BuildingInfo1.text = _info_text1 % [_selected_name, _selected_description]
+	var is_max := _selected_required_level == -1
+	var price_text := "-" if is_max else str(_selected_price)
+	var level_req_text := "-" if is_max else str(_selected_required_level)
+	var upgrading_text := "-" if is_max else "from level %s to %s" % [_selected_my_level, _selected_my_level + 1]
+	var action_text := " " if _selected_my_level == 0 else "Upgrading: " + upgrading_text
+	BuildingInfo2.text = _info_text2 % [price_text, level_req_text, action_text]
 
 # Opening the buy menu
 func _on_BuildingSpot_open_buy_menu(idx) -> void:
 	selected_idx = idx
 	_set_building_variables()
+	_set_building_info_text()
 	_switch_dialog_to(true)
 
+# Closing the menu
 func _on_BuildingDialogCloseButton_pressed() -> void:
 	selected_idx = -1
 	_switch_dialog_to(false)
@@ -39,6 +65,7 @@ func _on_BuildingDialogCloseButton_pressed() -> void:
 	NotEnoughLevelMessage.visible = false
 	MaxLevelMessage.visible = false
 
+# Buying the building
 func _on_BuildingDialogAcceptButton_pressed() -> void:
 	match _is_required_level_and_price():
 		Buying.POSSIBLE:
@@ -56,6 +83,7 @@ func _on_BuildingDialogAcceptButton_pressed() -> void:
 		_:
 			print("!ERROR! Buying didn't go as planned !ERROR!")
 
+# Checks the required level and price, also max level
 func _is_required_level_and_price() -> int:
 	if Planet.global_stuff.cosmic_credits_count < _selected_price:
 		return Buying.NOT_ENOUGH_CC
@@ -67,12 +95,15 @@ func _is_required_level_and_price() -> int:
 		return Buying.POSSIBLE
 	else:
 		return Buying.NOT_ENOUGH_LVL
-		
+
+# Opens / closes the dialog	
 func _switch_dialog_to(on: bool) -> void:
 	get_parent().get_node("LeftRotateButton").visible = !on
 	get_parent().get_node("RightRotateButton").visible = !on
-	BuildingInfo.visible = on
+	BuildingInfo1.visible = on
+	BuildingInfo2.visible = on
 	get_parent().get_node("ViewportContainer/Viewport/PlanetRotationNode").visible = !on
 	visible = on
-	
+
+# Different buying possibilities that can occur
 enum Buying {POSSIBLE, NOT_ENOUGH_CC, NOT_ENOUGH_LVL, MAX_LVL}
