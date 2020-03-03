1ad61a8b4fc44154fc44ef6689f8252f1a1b1556
Vojtěch Březina <26902688+VojtechBrezina@users.noreply.github.com>
2020-03-03 13:57:36 +0100

    ms

--- MiningScreen/GameBoard.gd
+++ MiningScreen/GameBoard.gd
@@ -12,7 +12,7 @@ enum _tiles{
 	STONE = 6
 }
 
-export(int) var _INIT_SIZE: int = 5
+export(int) var _INIT_SIZE: int = 50
 
 export(float) var _tile_size: float
 
@@ -23,8 +23,13 @@ var _max_y: int
 
 func _on_MiningScreen_game_started() -> void:
 	clear()
-	for i in range(0, _INIT_SIZE + 1):
+	for i in range(-1, _INIT_SIZE + 1):
 		_set_tile(i, _INIT_SIZE, _tiles.HIDEN)
+		_set_tile(-1, i, _tiles.HIDEN)
+		_set_tile(_INIT_SIZE, i, _tiles.HIDEN)
+	for i in range(0, _INIT_SIZE):
+		for j in range(0, _INIT_SIZE):
+			_put_random_color(i, j)
 
 func _random_color() -> int:
 	return randi() % 4 + 1
--- MiningScreen/MiningScreen.gd
+++ MiningScreen/MiningScreen.gd
@@ -1,12 +1,13 @@
 extends MarginContainer
 
 signal game_started
+signal setup(global_stuff)
 
 var _global_stuff: GlobalStuff
 
 func _ready() -> void:
 	_global_stuff = get_parent() as GlobalStuff
-	$VBoxContainer/Header/ResourcesBar.setup(_global_stuff)
+	emit_signal("setup", _global_stuff)
 	emit_signal("game_started")
 
 func _on_BackButton_pressed():
