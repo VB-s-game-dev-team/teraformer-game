09ec8f1c05ccbd2ea5f9161ba698337bd9144dd6
Filipino9507 <fildil111@gmail.com>
2020-03-03 13:58:35 +0100

    Volf je kretén
    Volf je kretén Volf je kretén Volf je kreténVolf je kreténVolf je kreténVolf je kreténVolf je kreténv

--- Planet/PlanetRotationNode.gd
+++ Planet/PlanetRotationNode.gd
@@ -2,24 +2,26 @@ extends Node2D
 
 var _planet_rotation_speed := 1.0
 var _planet_rotation_direction := 0
-var _planet_rotation_duration := 750.0
-var _rotation_timer := OS.get_ticks_msec()
 var _was_rotating := false
 var rotation_position := 0
 
+var _total_delta := 0.0
+var _max_delta := 1.0  # Duration
+
 func _process(delta: float) -> void:
-	if _rotation_timer + _planet_rotation_duration - 1000 * delta > OS.get_ticks_msec():
+	if _total_delta <= _max_delta:
+		_total_delta += delta
 		rotate(delta * _planet_rotation_direction * _planet_rotation_speed * PI / 2)
 	elif _was_rotating:
 		_terminate_rotation()
 
 # Set rotation variables and disable input	
 func _initiate_rotation(new_direction: int)	-> void:
+	_total_delta = 0.0
 	_was_rotating = true
 	get_tree().get_root().set_disable_input(true)
 	_planet_rotation_direction = new_direction
-	_planet_rotation_speed = 1000.0 / _planet_rotation_duration
-	_rotation_timer = OS.get_ticks_msec()
+	_planet_rotation_speed = 1.0 / _max_delta
 	rotation_position = (rotation_position + new_direction) % 4
 
 # Fix any rotation errors and enable input 
