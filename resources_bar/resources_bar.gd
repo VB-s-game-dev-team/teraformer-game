fa1ce14a735e9c8c14903c6fe93153c73e98f2b6
Filipino9507 <fildil111@gmail.com>
2020-03-05 13:45:20 +0100

    call DEferred
    ZULUL

--- resources_bar/resources_bar.gd
+++ resources_bar/resources_bar.gd
@@ -8,22 +8,22 @@ onready var LevelValue = $MarginContainer/LabelContainer/LevelValue
 onready var XPValue = $MarginContainer/LabelContainer/XPValue
 
 func _ready() -> void:
-	pass
-	
-func setup(g: GlobalStuff) -> void:
-	_global_stuff = g
-	g.connect("cosmic_credits_count_changed", self, "_update_values")
-	g.connect("star_dust_count_changed", self, "_update_values")
-	g.connect("experience_changed", self, "_update_values")
-	g.connect("level_changed", self, "_update_values")
+	_global_stuff = get_tree().root.get_child(0)
+	call_deferred("setup")
+
+func setup() -> void:
+	_global_stuff.connect("cosmic_credits_count_changed", self, "_update_values")
+	_global_stuff.connect("star_dust_count_changed", self, "_update_values")
+	_global_stuff.connect("experience_changed", self, "_update_values")
+	_global_stuff.connect("level_changed", self, "_update_values")
 	_update_values()
-	
+
 func _update_values():
 	CosmicCreditsValue.text = str(_global_stuff.cosmic_credits_count)
 	StarDustValue.text = str(_global_stuff.star_dust_count)
 	LevelValue.text = str(_global_stuff.level)
 	XPValue.text = str(_global_stuff.experience)
-	
+
 # Sets the values according to the global variables
 # Names like cosmic credits and star dust are temporary, but in any case:
 # star dust ... more rare / premium currency
